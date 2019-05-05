e236df4947c138fd0f225e34520bc8ed2222549a
Gobinda Deb <gdeb089@gmail.com>
2019-05-05 17:24:38 +0530

    Share Implemented

--- app/src/main/java/com/nytimes/sample/ui/news/DetailActivity.kt
+++ app/src/main/java/com/nytimes/sample/ui/news/DetailActivity.kt
@@ -1,38 +1,78 @@
 package com.nytimes.sample.ui.news
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.nytimes.sample.R
+import androidx.appcompat.widget.ShareActionProvider
 import com.nytimes.sample.data.model.News
 import kotlinx.android.synthetic.main.activity_details.*
 import kotlinx.android.synthetic.main.content_details.*
+import android.content.Intent
+import androidx.core.view.MenuItemCompat
+import com.nytimes.sample.R
+import timber.log.Timber
+
 
 class DetailActivity : AppCompatActivity() {
 
+    private var shareProvider: ShareActionProvider? = null
+
+    private lateinit var news: News;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_details)
         setSupportActionBar(detail_toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        val news = intent.getParcelableExtra<News>("news")
+        news = intent.getParcelableExtra<News>("news")
 
         details_data.text = news.title
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.details, menu)
+        menu.findItem(R.id.action_share).also { menuItem ->
+            shareProvider = MenuItemCompat.getActionProvider(menuItem)/*menuItem.actionProvider*/ as? ShareActionProvider
+            /*menuItem.setOnMenuItemClickListener {
+                val intent = Intent(Intent.ACTION_SEND).also {
+                    it.type = "text/plain"
+                    it.putExtra(Intent.EXTRA_TEXT, news.url)
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-       return when(item!!.itemId) {
-           android.R.id.home -> {
-               finish()
-              return true
-           }else -> super.onOptionsItemSelected(item)
+                }
+                shareProvider?.setShareIntent(intent)
+                true
+            }*/
+
         }
+
+        return true
     }
 
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Timber.d("Option item clicked")
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            R.id.action_share -> {
+                val intent = Intent(Intent.ACTION_SEND).also {
+                    it.type = "text/plain"
+                    it.putExtra(Intent.EXTRA_TEXT, news.url)
+
+                }
+                shareProvider?.setShareIntent(intent)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
 
 }
